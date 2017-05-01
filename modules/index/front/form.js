7b8e33a7abc5fbe7d283ea097e050d59888ec018
Ross Basarevych <basarevych@gmail.com>
2017-05-01 17:35:48 +0300

    Displaying the form

---
+++ modules/index/front/form.js
@@ -0,0 +1,36 @@
+'use strict';
+
+export class Form {
+    constructor(data) {
+        this.data = data;
+    }
+
+    static reset(el) {
+        el.find('.form-group').removeClass('has-danger');
+        el.find('.form-control').removeClass('form-control-danger');
+        el.find('.errors').empty();
+    }
+
+    update(el) {
+        this.constructor.reset(el);
+
+        for (let field of Object.keys(this.data)) {
+            let fieldEl = el.find(`[name="${field}"]`);
+            if (!fieldEl.length)
+                continue;
+
+            fieldEl.val(this.data[field].value);
+
+            if (!this.data[field].valid) {
+                fieldEl.addClass('form-control-danger');
+
+                let groupEl = fieldEl.parents('.form-group');
+                groupEl.addClass('has-danger');
+
+                let errorsEl = groupEl.find('.errors');
+                for (let error of this.data[field].errors)
+                    errorsEl.append($('<div class="form-control-feedback"></div>').text(error))
+            }
+        }
+    }
+}
--- modules/index/front/index.js
+++ modules/index/front/index.js
@@ -1,10 +1,24 @@
+import 'bootstrap';
 import 'styles/index.scss';
+import { Form } from 'form';
+
+'use strict';
+
+let signInModal;
+
+$(() => {
+    signInModal = $('#signInModal');
+    signInModal.on('hidden.bs.modal', () => {
+        Form.reset(signInModal);
+    });
+});
 
 export function loginSubmit() {
     'use strict';
 
-    $.post('/login', $('#signInModal').find('form').serialize(), data => {
-        console.log(data);
+    $.post('/login', signInModal.find('form').serialize(), data => {
+        let form = new Form(data.form);
+        form.update(signInModal);
     });
 
     return false;
